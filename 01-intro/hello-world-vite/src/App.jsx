39a389fc25bfe301225bfe088f1ea930f8a9a4b2
Mahonri Maia <mahonri.maia@gmail.com>
2024-07-29 00:24:32 -0300

    Upload do desafio 1

---
+++ 01-intro/hello-world-vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello World Vite</h1>
+      <p>
+        This is a simple paragraph line.
+      </p>
+    </>
+  )
+}
+
+export default App
